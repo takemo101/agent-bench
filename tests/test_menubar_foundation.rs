2137084deafc25cd3d90c37f823db9fbe554c340
takemo101 <takemo101@gmail.com>
2026-01-04 18:54:09 +0900

    fix: テスト関数の重複定義を修正

--- tests/test_menubar_foundation.rs
+++ tests/test_menubar_foundation.rs
@@ -4,12 +4,3 @@ use pomodoro::menubar::TrayIconManager;
 fn test_tray_icon_manager_structure() {
     let _ = TrayIconManager::new();
 }
-
-#[test]
-fn test_tray_icon_manager_methods() {}
-
-#[test]
-fn test_tray_icon_manager_methods() {
-    // We can't actually instantiate it if new() panics or fails (e.g. no icon file).
-    // But we verified signature above.
-}
