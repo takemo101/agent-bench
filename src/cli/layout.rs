2ade62201079f6af3f95112c5ae3c3a07033feeb
takemo101 <takemo101@gmail.com>
2026-01-10 09:00:08 +0900

    DisplayLayoutのスタブ作成

---
+++ src/cli/layout.rs
@@ -0,0 +1,11 @@
+// スタブ実装
+pub struct DisplayLayout {
+    // 仮のフィールド
+    pub lines: Vec<String>,
+}
+
+impl DisplayLayout {
+    pub fn new() -> Self {
+        Self { lines: Vec::new() }
+    }
+}
